b27cd75aee80556857078256e000a6a7feb775d4
Anjukdas <anjukollara@gmail.com>
2024-08-13 18:13:37 +0530

    3rd  commit

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let num = prompt("Enter number here");
 console.log(`number : ${num}`);
 
-if (num > 1000000) {
-    alert(` invalid number , Number must be less than or equal to 1000000`);
+if (num > 1000000 || num < 0) {
+    alert(` invalid number , Number must range 0 to 1000000`);
 } else {
     document.write(`Sum of entered number is : ${addNum(num)}`);
 }
 
 function addNum(number) {
     let sum = 0;
-    console.log(number.length)
-    for (let i = 0; i < number.length; i++) {
-        sum = sum + parseInt(number[i]);
+    for (let i = 0; i <= number; i++) {
+        sum = sum + i;
+        console.log({sum})
 
     }
     return sum;
